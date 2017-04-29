43da7cfdbc92d19dbb38f28ec19364bcb5fb1363
Jakub Tlalka <jakub@tlalka.net>
2017-04-29 19:22:12 +0200

    Replace Observable to Single

--- src/main/kotlin/net/tlalka/imager/core/GeoReader.kt
+++ src/main/kotlin/net/tlalka/imager/core/GeoReader.kt
@@ -4,42 +4,56 @@ import com.drew.imaging.ImageMetadataReader
 import com.drew.imaging.ImageProcessingException
 import com.drew.metadata.exif.GpsDirectory
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
 import net.tlalka.imager.data.GeoImage
 import java.io.File
-import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.FileTime
 
 class GeoReader {
 
     fun getGeoImage(image: File): Observable<GeoImage> {
-        return Observable
-                .just(image)
-                .flatMap { getGpsDirectory(image) }
-                .flatMap { getImageValue(image, it) }
+        return Single.zip(getGpsDirectory(image), getFileTime(image), BiFunction {
+            i1 : GpsDirectory, i2 : FileTime -> getImageValue(i1, i2, image)
+        }).toObservable()
     }
 
-    private fun getGpsDirectory(image: File): Observable<GpsDirectory> {
-        try {
-            val metadata = ImageMetadataReader.readMetadata(image)
-            val gpsDirectories = metadata.getDirectoriesOfType(GpsDirectory::class.java)
+    private fun getGpsDirectory(image: File): Single<GpsDirectory> {
+        return Single.create<GpsDirectory> {
+            run {
+                try {
+                    val metadata = ImageMetadataReader.readMetadata(image)
+                    val gpsDirectories = metadata.getDirectoriesOfType(GpsDirectory::class.java)
 
-            return Observable.fromIterable(gpsDirectories)
+                    it.onSuccess(gpsDirectories.firstOrNull())
 
-        } catch (e: ImageProcessingException) {
-            return Observable.empty()
-        } catch (e: IOException) {
-            return Observable.empty()
+                    throw ImageProcessingException("AAAA")
+
+                } catch (e: ImageProcessingException) {
+                    it.onError(e)
+                }
+            }
         }
     }
 
-    private fun getImageValue(image: File, gpsDirectory: GpsDirectory): Observable<GeoImage> {
+    private fun getFileTime(image: File) : Single<FileTime> {
+        return Single.create<FileTime> {
+            run {
+                val attr : BasicFileAttributes = Files.readAttributes(image.toPath(), BasicFileAttributes::class.java)
+                val time : FileTime = attr.creationTime() ?: FileTime.fromMillis(0)
+
+                it.onSuccess(time)
+            }
+        }
+    }
+
+    private fun getImageValue(gpsDirectory : GpsDirectory, fileTime: FileTime, image: File): GeoImage {
         val latitude = gpsDirectory.geoLocation.latitude
         val longitude = gpsDirectory.geoLocation.longitude
+        val createTime = fileTime.toMillis()
 
-        val attr : BasicFileAttributes = Files.readAttributes(image.toPath(), BasicFileAttributes::class.java)
-        val time = attr.creationTime()
-
-        return Observable.just(GeoImage(image, latitude, longitude, time.toMillis()))
+        return GeoImage(image, latitude, longitude, createTime)
     }
 }
